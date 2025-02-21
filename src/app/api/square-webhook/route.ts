75547975f3dab5131fcc7a348158a4a7c521bc17
MHanzala980 <muhammadhanzala980@gmail.com>
2025-02-21 18:44:52 +0500

    updated

--- src/app/api/square-webhook/route.ts
+++ src/app/api/square-webhook/route.ts
@@ -1,27 +1,27 @@
  import { NextRequest, NextResponse } from 'next/server';
-import { updateOrderStatus } from '@/lib/woocommerce';
+// import { updateOrderStatus } from '@/lib/woocommerce';
 
 export async function POST(req: NextRequest) {
-  const body = await req.text();
-  const event = JSON.parse(body);
+  // const body = await req.text();
+  // const event = JSON.parse(body);
 
-  if (event.type === 'payment.updated') {
-    const payment = event.data?.object?.payment;
-    const orderId = payment.order_id;
+  // if (event.type === 'payment.updated') {
+  //   const payment = event.data?.object?.payment;
+  //   const orderId = payment.order_id;
 
-    console.log(event, "event");
-    console.log("payment", payment);
-    if (payment && payment.status === 'COMPLETED') {
-      try {
-        await updateOrderStatus(orderId, 'completed');
-        console.log(`Order ${payment.order_id} status updated to completed`);
-        return NextResponse.json({ success: true });
-      } catch (error) {
-        console.error('Error updating order status:', error);
-        return NextResponse.json({ error: 'Failed to update order status' }, { status: 500 });
-      }
-    }
-  }
+  //   console.log(event, "event");
+  //   console.log("payment", payment);
+  //   if (payment && payment.status === 'COMPLETED') {
+  //     try {
+  //       await updateOrderStatus(orderId, 'completed');
+  //       console.log(`Order ${payment.order_id} status updated to completed`);
+  //       return NextResponse.json({ success: true });
+  //     } catch (error) {
+  //       console.error('Error updating order status:', error);
+  //       return NextResponse.json({ error: 'Failed to update order status' }, { status: 500 });
+  //     }
+  //   }
+  // }
 
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true, message: 'Webhook received' });
 }
